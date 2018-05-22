4a8a439b8189673217a38503d68ee3ded6221cfe
PeteFu <petefumail@gmail.com>
2018-05-22 18:20:03 +0800

    修改服务器
    修改服务器

--- src/config/option.js
+++ src/config/option.js
@@ -1,7 +1,7 @@
 export const Option = {
-  ApiServer: 'http://172.16.144.15:3000',
+  ApiServer: 'http://127.0.0.1:3000',
   // 图片服务器地址
-  ImgServer: 'http://172.16.144.15:3000/',
+  ImgServer: 'http://127.0.0.1:3000/',
   // 视频服务器地址
-  PlayServer: 'http://172.16.144.15:3000/api/play?fn='
+  PlayServer: 'http://127.0.0.1:3000/api/play?fn='
 }
